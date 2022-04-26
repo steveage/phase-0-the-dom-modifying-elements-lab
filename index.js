0e4992590e10970a06cf48563258fc9858498497
Mariusz <mariuszbia@yahoo.com>
2022-04-25 23:48:38 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainTag = document.getElementById("main");
+mainTag.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "My Name is the champion";
